a742b7a4eb9dffcaa23007951bd480b62ba3ce70
YongChao Yu <yuyongchao@itiger.com>
2021-08-03 16:04:01 +0800

    修改 转发抽象类

---
+++ src/main/java/com/github/yycYYYY/gtmd/interceptor/DefaultForwardInterceptor.java
@@ -0,0 +1,21 @@
+package com.github.yycYYYY.gtmd.interceptor;
+
+import io.netty.channel.Channel;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+
+/**
+ * @Author yuyongchao
+ **/
+public class DefaultForwardInterceptor implements ProxyInterceptor{
+
+    @Override
+    public void beforeRequest(Channel clientChannel, HttpRequest httpRequest, InterceptotPipeline pipeline) throws Exception {
+
+    }
+
+    @Override
+    public final void afterResponse(Channel clientChannel, Channel proxyChannel, HttpResponse httpResponse, InterceptotPipeline pipeline) throws Exception {
+
+    }
+}
